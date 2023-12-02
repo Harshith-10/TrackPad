75ccc8cb5291531ad895bab7b6c7ff7daa8c4cf9
Harshith <hdr8712865491@gmail.com>
2023-12-03 00:52:17 +0530

    Add further functionality.

--- app/src/main/java/harsh/trackpad/MainActivity.java
+++ app/src/main/java/harsh/trackpad/MainActivity.java
@@ -1,12 +1,13 @@
 package harsh.trackpad;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.Button;
 import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
@@ -14,27 +15,66 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 public class MainActivity extends AppCompatActivity {
-    private LinearLayout mouseArea;
-    private Button btn_mouse_left, btn_mouse_right;
-    private static int mouse_x = 0, mouse_y = 0;
+    private static float mouse_x = 0, mouse_y = 0;
     private static boolean mouse_lt = false, mouse_rt = false;
+
+    public static Thread mouseSenderThread;
+    public static MouseTransmitter mouseTransmitterRunnable;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
-        mouseArea = findViewById(R.id.area_mouse);
-        btn_mouse_left = findViewById(R.id.btn_mouse_left);
-        btn_mouse_right = findViewById(R.id.btn_mouse_right);
+        LinearLayout mouseArea = findViewById(R.id.area_mouse);
+        Button btn_mouse_lt = findViewById(R.id.btn_mouse_left);
+        Button btn_mouse_rt = findViewById(R.id.btn_mouse_right);
 
-        mouseArea.setOnTouchListener((v, event) -> {
-            int x = (int) event.getX();
-            int y = (int) event.getY();
-
-            return false;
+        mouseArea.setOnTouchListener((v, e) -> {
+            //v.getParent().requestDisallowInterceptTouchEvent(true);
+            float x = e.getX(), y = e.getY();
+            float max_x = v.getWidth(), max_y = v.getHeight();
+            mouse_x = x / max_x;
+            mouse_y = y / max_y;
+            if (mouse_x < 0) mouse_x = 0;
+            if (mouse_x > 1) mouse_x = 1;
+            if (mouse_y < 0) mouse_y = 0;
+            if (mouse_y > 1) mouse_y = 1;
+            if(e.getAction() == MotionEvent.ACTION_MOVE) System.out.println(mouse_x + " " + mouse_y);
+            // Todo: add touch events
+            return true;
         });
+
+        btn_mouse_lt.setOnTouchListener((v, e) -> {
+            mouse_lt = switch (e.getAction()) {
+                case MotionEvent.ACTION_DOWN -> true;
+                case MotionEvent.ACTION_UP -> false;
+                case MotionEvent.ACTION_MOVE -> mouse_lt;
+                default -> throw new IllegalStateException("Unexpected value: " + e.getAction());
+            };
+            Log.d(getLocalClassName(), "MLT: " + mouse_lt);
+            return true;
+        });
+
+        btn_mouse_rt.setOnTouchListener((v, e) -> {
+            mouse_rt = switch (e.getAction()) {
+                case MotionEvent.ACTION_DOWN -> true;
+                case MotionEvent.ACTION_UP -> false;
+                case MotionEvent.ACTION_MOVE -> mouse_rt;
+                default -> throw new IllegalStateException("Unexpected value: " + e.getAction());
+            };
+            Log.d(getLocalClassName(), "MRT: " + mouse_rt);
+            return true;
+        });
+
+        mouseTransmitterRunnable = new MouseTransmitter();
+        /*mouseSenderThread = new Thread(mouseSenderThread);
+        mouseSenderThread.start();*/
+
+        new Thread(mouseTransmitterRunnable).start();
+        Log.d(getLocalClassName(), "Thread start() called");
     }
 
     private static class MouseTransmitter implements Runnable {
@@ -44,12 +84,17 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run() {
             try (DatagramSocket socket = new DatagramSocket(PORT)) {
-                InetAddress hostAddress = InetAddress.getByName("add pc ip here");
+                InetAddress hostAddress = InetAddress.getByName("192.168.15.254");
+                Log.d(getClass().getName(), "Runnable Started");
+                long startMillis = System.currentTimeMillis(), n = 0;
                 while (keepRunning) {
                     byte[] data = new byte[10];
                     packMouseData(data);
                     DatagramPacket packet = new DatagramPacket(data, data.length, hostAddress, PORT);
                     socket.send(packet);
+                    //System.out.println(n++);
+                    while (System.currentTimeMillis() - startMillis < 34); // about 2 times a second
+                    startMillis = System.currentTimeMillis();
                 }
             } catch (IOException e) {
                 throw new RuntimeException(e);
@@ -57,9 +102,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         private void packMouseData(byte[] data) {
+            int temp_x, temp_y;
+            temp_x = Float.floatToIntBits(mouse_x);
+            temp_y = Float.floatToIntBits(mouse_y);
             for (int i = 0; i < 4; i++) {
-                data[i]   = (byte) (mouse_x >>> (i * 8));
-                data[i+4] = (byte) (mouse_y >>> (i * 8));
+                data[i]   = (byte) (temp_x >>> (i * 8));
+                data[i+4] = (byte) (temp_y >>> (i * 8));
             }
             data[9] = (byte) ((mouse_lt?2:0)|(mouse_rt?1:0));
         }
--- host-lib/src/main/java/harsh/trackpad/host/MouseControl.java
+++ host-lib/src/main/java/harsh/trackpad/host/MouseControl.java
@@ -1,21 +1,57 @@
 package harsh.trackpad.host;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
 public class MouseControl {
-    public static int mouse_x = 0, mouse_y = 0;
-    public static boolean mouse_lt = false, mouse_rt = false;
+    public static float mouse_x = 0, mouse_y = 0;
+    private static boolean mouse_lt = false, mouse_rt = false, pmouse_lt = false, pmouse_rt = false;
+    private static long lastMouseLt = 0, lastMouseRt = 0;
+
+    private static Robot robot;
 
     public static void main(String[] args) {
+        try {
+            robot = new Robot();
+        } catch (AWTException e) {
+            throw new RuntimeException(e);
+        }
         MouseEventReceiver eventReceiver = new MouseEventReceiver();
         Thread receiverThread = new Thread(eventReceiver);
         receiverThread.start();
     }
 
     public static void updateMouse(){
-        System.out.println(mouse_x + ", " + mouse_y + " cl: " + mouse_cl);
+        if (robot == null) return;
+        if(mouse_lt && (System.currentTimeMillis() - lastMouseLt) > 80){
+            if(!pmouse_lt) {
+                robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+                pmouse_lt = true;
+            }
+            lastMouseLt = System.currentTimeMillis();
+        } else {
+            if (pmouse_lt) {
+                robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+                pmouse_lt = false;
+            }
+        }
+
+        if(mouse_rt && (System.currentTimeMillis() - lastMouseRt) > 80){
+            if(!pmouse_rt) {
+                robot.mousePress(InputEvent.BUTTON2_DOWN_MASK);
+                pmouse_rt = true;
+            } else {
+                robot.mouseRelease(InputEvent.BUTTON2_DOWN_MASK);
+                pmouse_rt = false;
+            }
+            lastMouseRt = System.currentTimeMillis();
+        }
+
+        System.out.println(mouse_x + ", " + mouse_y);
         // do something
     }
 
@@ -40,13 +76,15 @@ public class MouseControl {
 
         private void parseMouseData(DatagramPacket packet) {
             byte[] data = packet.getData();
-            mouse_x = mouse_y = 0;
-            for (int i = 0; i < 4; i++) {
-                mouse_x <<= 8;
-                mouse_x |= data[i];
-                mouse_y <<= 8;
-                mouse_y |= data[i+4];
+            int temp_x = 0, temp_y = 0;
+            for (int i = 4; i >= 0; i--) {
+                temp_x <<= 8;
+                temp_x |= data[i];
+                temp_y <<= 8;
+                temp_y |= data[i+4];
             }
+            mouse_x = Float.intBitsToFloat(temp_x);
+            mouse_y = Float.intBitsToFloat(temp_y);
             mouse_rt = ((data[9] & 1) == 1);
             mouse_lt = ((data[9] & 2) == 2);
             updateMouse();
